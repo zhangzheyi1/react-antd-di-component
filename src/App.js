187b1e46e8c3a87a05e7b7cc4da88f73fd5f977e
zhangzheyi <1012584472@qq.com>
2021-07-26 15:32:24 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import "./index.css";
+import { Button, version } from "antd";
+import DiInput from './biz-dumb/DiInput';
+import React, { useState } from 'react';
 
 function App() {
+  const [inputValue, setInputValue] = useState('');
+  const [isLoading, onSearchLoading] = useState(false);
+  const stInput = { title: { width: 0 }, input: { width: 200 }, inputSize: 'middle' };
+  console.log('inputValue', inputValue);
+  console.log('isLoading', isLoading);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>antd version: {version}</h1>
+      <p>{inputValue}</p>
+      <br />
+      <DiInput pre required extraStyle={stInput} placeholder='请输入...' value={inputValue} onChange={e => setInputValue(e.target.value)}
+        isSearch={true} onSearch={() => onSearchLoading(true)} loading={isLoading}/>
+        <br/>
+      <Button onClick={() => onSearchLoading(false)}>stop Loading</Button>
     </div>
   );
 }
---
+++ src/types/dom.d.ts
@@ -0,0 +1,6 @@
+
+export type InputChangeEv =  React.ChangeEvent<HTMLInputElement>;
+
+export type KeyboardEv =  React.KeyboardEvent<HTMLInputElement>;
+
+export type MouseEv =  React.MouseEvent<HTMLInputElement>;
